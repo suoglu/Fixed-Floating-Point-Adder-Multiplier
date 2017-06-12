810a8fc9c55335c382fe9a513625f63503637622
suoglu <yigitsuoglu@icloud.com>
2017-06-12 15:21:27 +0300

    Floating multi finished

--- adder-mutiplier.v
+++ adder-mutiplier.v
@@ -75,7 +75,8 @@ module float_multi(num1, num2, result, overflow);
   wire [9:0] fra1, fra2; //hold fractions
   wire [11:0] float1; // true value of fra1 i.e. 1.ffffffffff
   wire [5:0] exSum, exFinalSum; //exponent sums
-  reg [11:0] float_res; //fraction result
+  wire [11:0] float_res; //fraction result
+  reg [11:0] mid[9:0], mid2[1:0];
 
   assign overflow = exFinalSum[5]; //extra digit of final sum exist at overflow
   //decode numbers
@@ -85,15 +86,35 @@ module float_multi(num1, num2, result, overflow);
   assign signr = (sign1 ^ sign2);
   assign exSum = (ex1 + ex2); //exponentials are added
   assign float1 = {2'b01, fra1};
+  assign float_res = float1 + mid2[1] + mid2[0]; //add mid2 terms and integer
+                                                // of num2
+  //if multipication of fractions cause shift
+  assign exFinalSum = (float_res[11] == 1) ? (exSum + 6'b1) : exSum;
 
+  always@* //create mids from fractions
+    begin
+      mid[0] = (float1 >> 10) & {16{fra2[0]}};
+      mid[1] = (float1 >> 9) & {16{fra2[1]}};
+      mid[2] = (float1 >> 8) & {16{fra2[2]}};
+      mid[3] = (float1 >> 7) & {16{fra2[3]}};
+      mid[4] = (float1 >> 6) & {16{fra2[4]}};
+      mid[5] = (float1 >> 5) & {16{fra2[5]}};
+      mid[6] = (float1 >> 4) & {16{fra2[6]}};
+      mid[7] = (float1 >> 3) & {16{fra2[7]}};
+      mid[8] = (float1 >> 2) & {16{fra2[8]}};
+      mid[9] = (float1 >> 1) & {16{fra2[9]}};
+    end
 
-
+  always@* //create mid2s from mids
+    begin
+      mid2[1] = mid[0] + mid[1] + mid[2] + mid[3] + mid[4];
+      mid2[0] = mid[5] + mid[6] + mid[7] + mid[8] + mid[9];
+    end
 
   always@* //Results
     begin
        result[15] = signr;
        result[14:10] = exFinalSum[4:0];
-
        //Both casex and if-else statements does same thing
        /*casex(float_res)
  				12'b01??????????: result[9:0] = float_res[9:0];
