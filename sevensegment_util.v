02b0819cf17f48017fdf2ea7f2d4c1efe2268de8
suoglu <yigitsuoglu@icloud.com>
2017-06-14 10:16:59 +0300

    seven segment controller added

--- sevensegment_util.v
+++ sevensegment_util.v
@@ -1,4 +1,5 @@
 // Yigit Suoglu
+//This file contains modules about seven segment display
 /*
  *   At ssdDecode module output signals in form of abcdefg corresponding:
  *      a
@@ -9,7 +10,8 @@
  */
 `timescale 1ns / 1ps
 
-module ssdDecode (in, abcdefg);
+//Converts 4 bit input to hex abcdefg format provided above
+module ssdDecode(in, abcdefg);
   parameter zero = 7'b0000001, one = 7'b1001111, two = 7'b0010010;
   parameter thr = 7'b0000110, four = 7'b1001100, five = 7'b0100100;
   parameter six = 7'b0100000, svn = 7'b0001111, eght = 7'b0000000;
@@ -41,3 +43,104 @@ module ssdDecode (in, abcdefg);
     end
 
 endmodule // ssdDecode
+
+ // This module controls seven segment display of Basys 3
+// This module designed for 100 MHz clock
+module ssd_cntr(clk, rst, a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1,d2,d3,e0,e1,e2,e3,
+f0,f1,f2,f3,g0,g1,g2,g3,a,b,c,d,e,f,g,an0,an1,an2,an3);
+
+  input clk, rst;
+  input a0,a1,a2,a3,b0,b1,b2,b3,c0,c1,c2,c3,d0,d1;  // abcdefg signals for
+  input f2,f3,g0,g1,g2,g3,d3,e0,e1,e2,e3,f0,f1,d2; // corresponding hex
+  output reg a,b,c,d,e,f,g,an0,an1,an2,an3; // connected to ssd interface
+
+  wire stateClk;
+  reg [1:0] state;
+  reg [15:0] counter; //655.36µs or ~1.526 kHz
+
+  assign stateClk = counter[15]; //state clock determined by MSB of counter
+
+  //Note that: both state and counter will warp 11.. to 00.. at max
+  //state transactions
+  always@(posedge stateClk or posedge rst)
+    begin
+      if(rst)
+        state <= 2'b0;
+      else
+        state <= state + 2'b1;
+    end
+
+  //counter
+  always@(posedge clk or posedge rst)
+    begin
+      if(rst)
+        counter <= 16'b0;
+      else
+        counter <= state + 16'b1;
+    end
+
+  //abcdefg and anode routing
+  always@*
+    begin
+      case (state)
+        2'b0:
+          begin
+            a = a0;
+            b = b0;
+            c = c0;
+            d = d0;
+            e = e0;
+            f = f0;
+            g = g0;
+            an0 = 0;
+            an1 = 1;
+            an2 = 1;
+            an3 = 1;
+          end
+        2'b1:
+          begin
+            a = a1;
+            b = b1;
+            c = c1;
+            d = d1;
+            e = e1;
+            f = f1;
+            g = g1;
+            an0 = 1;
+            an1 = 0;
+            an2 = 1;
+            an3 = 1;
+          end
+        2'b10:
+          begin
+            a = a2;
+            b = b2;
+            c = c2;
+            d = d2;
+            e = e2;
+            f = f2;
+            g = g2;
+            an0 = 1;
+            an1 = 1;
+            an2 = 0;
+            an3 = 1;
+          end
+        2'b11:
+          begin
+            a = a3;
+            b = b3;
+            c = c3;
+            d = d3;
+            e = e3;
+            f = f3;
+            g = g3;
+            an0 = 1;
+            an1 = 1;
+            an2 = 1;
+            an3 = 0;
+          end
+
+      endcase
+    end
+
+endmodule // ssd
