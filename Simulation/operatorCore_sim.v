5465503060ffd6a02dcc55bb217cad114d0fe27b
suoglu <suoglu@icloud.com>
2020-04-09 16:35:13 +0200

    code for dump file output added

--- Simulation/operatorCore_sim.v
+++ Simulation/operatorCore_sim.v
@@ -42,14 +42,43 @@ module operatorCore_sim();
         begin
             num1 <= 16'd27;
             num2 <= 16'd42;
-           #250
+            #250
             num1 <= 16'd561;
             num2 <= 16'd158;
-           #250
-           num1 <=  16'b1010101010001110;
-           num2 <= 16'b0101011100100110;
-           #250
-           num1 <= 16'b1111110110111010;
-           num2 <= 16'b0100111001001111;
+            #250
+            num1 <=  16'b1010101010001110;
+            num2 <= 16'b0101011100100110;
+            #250
+            num1 <= 16'b1111110110111010;
+            num2 <= 16'b0100111001001111;
+            #250
+            $finish;
+        end
+    initial 
+        begin  
+            $dumpfile("output_waveform.vcd"); 
+            $dumpvars(0, num1);
+            $dumpvars(1, num2);
+            $dumpvars(2, fixA);
+            $dumpvars(3, fixM);
+            $dumpvars(4, floM);
+            $dumpvars(5, floA);
+            $dumpvars(6, overflow);
+            $dumpvars(7, num1_int);
+            $dumpvars(8, num1_flo);
+            $dumpvars(9, num2_int);
+            $dumpvars(10, num2_flo);
+            $dumpvars(11, num1_sign);
+            $dumpvars(12, num1_exp);
+            $dumpvars(13, num1_frac);
+            $dumpvars(14, num2_sign);
+            $dumpvars(15, num2_exp);
+            $dumpvars(16, num2_frac);
+            $dumpvars(17, floA_sign);
+            $dumpvars(18, floA_exp);
+            $dumpvars(19, floA_frac);
+            $dumpvars(20, floM_sign);
+            $dumpvars(21, floM_exp);
+            $dumpvars(22, floM_frac);
         end
 endmodule
